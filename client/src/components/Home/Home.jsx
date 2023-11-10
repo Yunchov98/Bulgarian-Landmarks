4ba2789b69ccedd3501efc1a6418f503046a82e4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-10 22:29:46 +0200

    Add home component

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import styles from './Home.module.css';
+
+export default function Home() {
+    return (
+        <>
+            <section className={styles['wrapper']}>
+                <article>
+                    <h2 className={styles['heading']}>Explore Bulgaria</h2>
+                    <p>Here you can find some lanmarks in Bulgaria</p>
+                    <p>
+                        Bulgaria is a little country in Europe with beautiful
+                        nature
+                    </p>
+                </article>
+
+                <section className={styles['img-wrapper']}>
+                    <ul>
+                        <li>
+                            <img src="images/R.jpg" alt="img" />
+                            <p>Aleksander Nevski</p>
+                        </li>
+                        <li>
+                            <img src="images/Belogradchik.jpg" alt="img" />
+                            <p>Belogradchik</p>
+                        </li>
+                        <li>
+                            <img src="images/Shipka.jpg" alt="img" />
+                            <p>Shipka</p>
+                        </li>
+                    </ul>
+                </section>
+            </section>
+
+            <section className={styles['about-landmarks']}>
+                {/* TODO */}
+            </section>
+        </>
+    );
+}
