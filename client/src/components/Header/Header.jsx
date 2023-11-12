4c412722c9bf6a412dc0574c1a9312d07f11a7b4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-12 21:58:04 +0200

    Add navlink

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,41 @@
-import styles from './Header.module.css'
+import { NavLink } from 'react-router-dom';
+
+import styles from './Header.module.css';
 
 export default function Header() {
     return (
-            <header className={styles['site-header']}>
-            <h2 className={styles['site-name']}>Bulgarian Landmarks</h2>
+        <header className={styles['site-header']}>
+            <NavLink to={'/'} className={styles['site-name']}>
+                <h2>Bulgarian Landmarks</h2>
+            </NavLink>
             <form className={styles['search-form']}>
-                <input className={styles['search-bar']} type="search" name="search" id="search" placeholder='Search...' />
-                <input className={styles['search-button']} type="button" value="Search" />
+                <input
+                    className={styles['search-bar']}
+                    type="search"
+                    name="search"
+                    id="search"
+                    placeholder="Search..."
+                />
+                <input
+                    className={styles['search-button']}
+                    type="button"
+                    value="Search"
+                />
             </form>
             <nav className={styles['main-nav']}>
                 <ul>
-                    <li><a href="#">Catalog</a></li>
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Profile</a></li>
+                    <li>
+                        <NavLink to="/catalog">Catalog</NavLink>
+                    </li>
+                    <li>
+                        <a href="#">About us</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Profile</a>
+                    </li>
                 </ul>
             </nav>
         </header>
