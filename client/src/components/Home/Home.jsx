85f0cb56c98237e173ef37949e6f5a4114a3c4a7
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-12 23:49:29 +0200

    Add components

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,68 +1,15 @@
-import styles from './Home.module.css';
+import Hero from './sections/Hero/Hero';
+import AboutBulgaria from './sections/AboutBulgaria/AboutBulgaria';
+import BulgarianHistory from './sections/BulgarianHistory/BulgarianHistory';
 
 export default function Home() {
     return (
         <>
-            <section className={styles['wrapper']}>
-                <article>
-                    <h2 className={styles['heading']}>Explore Bulgaria</h2>
-                    <p>Here you can find some lanmarks in Bulgaria</p>
-                    <p>
-                        Bulgaria is a little country in Europe with beautiful
-                        nature
-                    </p>
-                </article>
+            <Hero />
 
-                <section className={styles['img-wrapper']}>
-                    <ul>
-                        <li>
-                            <img src="images/R.jpg" alt="img" />
-                            <p>Aleksander Nevski</p>
-                        </li>
-                        <li>
-                            <img src="images/Belogradchik.jpg" alt="img" />
-                            <p>Belogradchik</p>
-                        </li>
-                        <li>
-                            <img src="images/Shipka.jpg" alt="img" />
-                            <p>Shipka</p>
-                        </li>
-                    </ul>
-                </section>
-            </section>
+            <AboutBulgaria />
 
-            <section className={styles['about-bulgaria']}>
-                <h3>About Bulgaria</h3>
-                <section className={styles['banner']}>
-                    <div className={styles['item']}>
-                        <div className={styles['fa-icon']}>
-                            <i className="fa-solid fa-landmark"></i>
-                        </div>
-                        <p className={styles['item-text']}>
-                            352 natural sights
-                        </p>
-                    </div>
-
-                    <div className={styles['item']}>
-                        <div className={styles['fa-icon']}>
-                            <i className="fa-solid fa-person"></i>
-                        </div>
-                        <p className={styles['item-text']}>
-                            Over 6.5 milion people
-                        </p>
-                    </div>
-
-                    <div className={styles['item']}>
-                        <div className={styles['fa-icon']}>
-                        <i className="fa-solid fa-up-right-and-down-left-from-center"></i>
-                        </div>
-                        <p className={styles['item-text']}>
-                            Area: 111 000 km<sup>2</sup>
-                        </p>
-                    </div>
-                </section>
-            </section>
-           
+            <BulgarianHistory />
         </>
     );
 }
