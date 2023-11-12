ae71fcf6390cdb9fe95cf302d355b99689a8dd40
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-12 22:00:02 +0200

    Add catalog component

---
+++ client/src/components/Catalog/Catalog.jsx
@@ -0,0 +1,130 @@
+import styles from './Catalog.module.css';
+
+export default function Catalog() {
+    return (
+        <section>
+            <h2 className={styles['catalog-heading']}>Landmarks</h2>
+            <div className={styles['content-wrapper']}>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/R.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+                <div className={styles['item-wrapper']}>
+                    <div className={styles['media']}>
+                        <img src="images/Shipka.jpg" alt="Shipka" />
+                    </div>
+                    <div className={styles['item']}>
+                        <h3 className={styles['item-name']}>Shipka</h3>
+                        <p className={styles['item-description']}>
+                            Shipka is one of the most famous landmraks in
+                            Bulgaira
+                        </p>
+                        <p className={styles['author']}>Posted by: Pesho</p>
+                    </div>
+                    <div className={styles['fs-icon']}>
+                    <i className="fa-solid fa-heart"></i>
+                    <p className={styles['like-text']}>Like</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
