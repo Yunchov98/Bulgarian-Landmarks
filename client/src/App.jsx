75c924747a52e1f75e3003e40adaf02c3804d6d8
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-12 22:00:36 +0200

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Catalog from './components/Catalog/Catalog';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
@@ -9,8 +13,13 @@ function App() {
             <Header />
 
             <main className="site-content">
-                <Home />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/catalog' element={<Catalog />} />
+                </Routes>
             </main>
+
+            <Footer />
         </>
     );
 }
