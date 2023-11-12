91bfc95d0ef5a522d1c2ea6c9528948f679d4a83
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-12 21:59:18 +0200

    Add footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles['site-footer']}>
+            <p>All rights reserved &copy;</p>
+        </footer>
+    );
+}
