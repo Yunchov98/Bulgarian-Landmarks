ce497e303ae812fb37530ef900886e35d5e4f214
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-16 20:54:21 +0200

    Update user-section

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,7 +7,11 @@ import styles from './Header.module.css';
 export default function Header() {
     return (
         <header className={styles['site-header']}>
-            <Link onClick={() => toTheTop('instant')} to={'/'} className={styles['site-name']}>
+            <Link
+                onClick={() => toTheTop('instant')}
+                to={'/'}
+                className={styles['site-name']}
+            >
                 <h2>Bulgarian Landmarks</h2>
             </Link>
             <form className={styles['search-form']}>
@@ -25,18 +29,52 @@ export default function Header() {
                 />
             </form>
             <nav className={styles['main-nav']}>
-                <ul>
-                    <li>
-                        <Link to="/catalog">Catalog</Link>
+                <ul className={styles['main-ul']}>
+                    <li className={styles['main-ul-li']}>
+                        <div className={styles['menu']}>
+                            <i className="fa-solid fa-arrows-to-circle"></i>
+                        </div>
+                        <ul className={styles['inner-nav']}>
+                            <li className={styles['inner-nav-li']}>
+                                <div className={styles['catalog-wrapper']}>
+                                    <i className="fa-solid fa-landmark-dome"></i>
+                                    <Link to={'/catalog'}>Catalog</Link>
+                                </div>
+                            </li>
+                            <li className={styles['inner-nav-li']}>
+                                <div>
+                                    <i className="fa-solid fa-address-card"></i>
+                                    <Link to={'#'}>About us</Link>
+                                </div>
+                            </li>
+                            <li className={styles['inner-nav-li']}>
+                                <div className={styles['create-wrapper']}>
+                                    <i className="fa-solid fa-pen"></i>
+                                    <Link to={'#'}>Create Post</Link>
+                                </div>
+                            </li>
+                        </ul>
                     </li>
-                    <li>
-                        <a href="#">About us</a>
-                    </li>
-                    <li>
-                        <a href="#">Blog</a>
-                    </li>
-                    <li>
-                        <a href="#">Profile</a>
+                    <li className={styles['main-ul-li']}>
+                        <img
+                            src="https://th.bing.com/th/id/R.6b0022312d41080436c52da571d5c697?rik=ejx13G9ZroRrcg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fuser-png-icon-young-user-icon-2400.png&ehk=NNF6zZUBr0n5i%2fx0Bh3AMRDRDrzslPXB0ANabkkPyv0%3d&risl=&pid=ImgRaw&r=0"
+                            alt=""
+                        />
+                        <ul className={styles['inner-nav']}>
+                            <li className={styles['user-info']}>
+                                <Link to={'/catalog'}>
+                                    <img
+                                        src="https://th.bing.com/th/id/R.6b0022312d41080436c52da571d5c697?rik=ejx13G9ZroRrcg&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fuser-png-icon-young-user-icon-2400.png&ehk=NNF6zZUBr0n5i%2fx0Bh3AMRDRDrzslPXB0ANabkkPyv0%3d&risl=&pid=ImgRaw&r=0"
+                                        alt=""
+                                    />
+                                    <p>User Name</p>
+                                </Link>
+                                <div className={styles['logout']}>
+                                    <i className="fa-solid fa-right-from-bracket"></i>
+                                    <p>Logout</p>
+                                </div>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </nav>
