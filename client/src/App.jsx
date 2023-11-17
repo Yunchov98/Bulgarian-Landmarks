666d876680f9ceed7619179e3022a6a06be65f27
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-17 17:35:40 +0200

    Add create-post component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
 import Footer from './components/Footer/Footer';
 import ToTheTopButton from './components/ToTheTopButton/ToTheTopButton';
+import CreatePost from './components/CreatePost/CreatePost';
 
 function App() {
     return (
@@ -15,8 +16,9 @@ function App() {
 
             <main className="site-content">
                 <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/catalog' element={<Catalog />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/create-post" element={<CreatePost />} />
                 </Routes>
 
                 <ToTheTopButton />
