b9640b544164a99ee9fa63046094b3fbef01455d
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-17 14:05:54 +0200

    Add state and render catalog items dynamically

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,18 +1,26 @@
-import styles from './Catalog.module.css';
+import { useEffect, useState } from 'react';
 
 import CatalogItem from './CatalogItem/CatalogItem';
+import * as postService from '../../services/postService';
+
+import styles from './Catalog.module.css';
 
 export default function Catalog() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        postService.getAllPosts().then((result) => {
+            setPosts(result.reverse());
+            console.log(result);
+        });
+    }, []);
     return (
         <section className={styles['catalog-section']}>
             <h2 className={styles['catalog-heading']}>Landmarks</h2>
             <div className={styles['content-wrapper']}>
-                <CatalogItem />
-                <CatalogItem />
-                <CatalogItem />
-                <CatalogItem />
-                <CatalogItem />
-                <CatalogItem />
+                {posts.map((post) => (
+                    <CatalogItem key={post._id} {...post} />
+                ))}
             </div>
         </section>
     );
