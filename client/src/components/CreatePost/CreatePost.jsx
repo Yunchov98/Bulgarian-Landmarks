771c81538037df9af93a67e5b9ce32b8f91488f3
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-24 21:52:11 +0200

    Update formatting

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import {DevTool} from '@hookform/devtools'
+import { DevTool } from '@hookform/devtools';
 
 import { toBase64 } from '../../utils/base64';
 import * as postService from '../../services/postService';
@@ -11,8 +11,8 @@ import dateConverter from '../../utils/dateConverter';
 export default function CreatePost() {
     const navigate = useNavigate();
 
-    const form = useForm({mode: 'onBlur'});
-    const { register, control ,handleSubmit, formState } = form;
+    const form = useForm({ mode: 'onBlur' });
+    const { register, control, handleSubmit, formState } = form;
     const { errors } = formState;
 
     const onSubmit = async (data) => {
@@ -37,29 +37,42 @@ export default function CreatePost() {
                     onSubmit={handleSubmit(onSubmit)}
                 >
                     <div className={styles['form-item']}>
-                        <textarea  {...register('description', {
-                            required: 'Description is required'
-                        })} name="description" id="description" cols="50" rows="6" placeholder='Write some description here...'></textarea>
-                        <p className={styles['error']}>{errors.description?.message}</p>
+                        <textarea
+                            {...register('description', {
+                                required: 'Description is required',
+                            })}
+                            name="description"
+                            id="description"
+                            cols="50"
+                            rows="6"
+                            placeholder="Write some description here..."
+                        ></textarea>
+                        <p className={styles['error']}>
+                            {errors.description?.message}
+                        </p>
                     </div>
                     <div className={styles['form-item']}>
                         <div className={styles['file-wrapper']}>
-                            <span className={styles['drop-title']}>Drop files here</span>
+                            <span className={styles['drop-title']}>
+                                Drop files here
+                            </span>
                             <i className="fa-solid fa-down-long"></i>
                             <input
                                 className={styles['file-input']}
                                 {...register('file', {
                                     required: 'Image is required',
-                                    minLength: 1
+                                    minLength: 1,
                                 })}
                                 type="file"
                                 name="file"
                                 id="file"
                             />
                         </div>
-                        <p className={styles['error']}>{errors.file?.message}</p>
+                        <p className={styles['error']}>
+                            {errors.file?.message}
+                        </p>
                     </div>
-                    
+
                     <div className={styles['form-item']}>
                         <button>Create Post</button>
                     </div>
