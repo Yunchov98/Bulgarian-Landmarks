b6d922b2e87c15debff8f2017730fbd08012d468
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-27 20:15:50 +0200

    Add paths, endpoints and form values

---
+++ client/src/core/environments/constants.js
@@ -0,0 +1,30 @@
+export const baseUrl = 'http://localhost:3030/';
+
+export const PATH = {
+    home: '/',
+    login: '/login',
+    register: '/register',
+    logout: '/logout',
+    createPost: '/create-post',
+    catalog: '/catalog',
+};
+
+export const endpoints = {
+    login: 'users/login',
+    register: 'users/register',
+    logout: 'users/logout',
+};
+
+export const LoginFormKeys = {
+    Email: 'email',
+    Password: 'password',
+};
+
+export const RegisterFormKeys = {
+    Email: 'email',
+    Username: 'username',
+    Age: 'age',
+    Gender: 'gender',
+    Password: 'password',
+    ConfirmPassword: 'confirmPassword',
+};
