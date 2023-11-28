4872488a141338cda48188ad5a55bc64b5a56451
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-28 21:24:31 +0200

    Add auth context log in, logout, register and route guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,55 @@
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import { PATH } from './core/environments/constants';
 
+import { AuthProvider } from './contexts/authContext';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
-import Footer from './components/Footer/Footer';
-import ToTheTopButton from './components/ToTheTopButton/ToTheTopButton';
 import CreatePost from './components/CreatePost/CreatePost';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Footer from './components/Footer/Footer';
+import ToTheTopButton from './components/ToTheTopButton/ToTheTopButton';
+import AuthGuard from './guards/AuthGuard';
+import LoggedInGuard from './guards/LoggedInGuard';
+import Logout from './components/Logout/Logout';
 
 function App() {
     return (
-        <>
-            <Header />
+        <AuthProvider>
+            <>
+                <Header />
 
-            <main className="site-content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/create-post" element={<CreatePost />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
+                <main className="site-content">
+                    <Routes>
+                        <Route path={PATH.home} element={<Home />} />
+                        <Route path={PATH.catalog} element={<Catalog />} />
 
-                <ToTheTopButton />
-            </main>
+                        <Route element={<AuthGuard />}>
+                            <Route
+                                path={PATH.createPost}
+                                element={<CreatePost />}
+                            />
+                            <Route path={PATH.logout} element={<Logout />} />
+                        </Route>
 
-            <Footer />
-        </>
+                        <Route element={<LoggedInGuard />}>
+                            <Route path={PATH.login} element={<Login />} />
+                            <Route
+                                path={PATH.register}
+                                element={<Register />}
+                            />
+                        </Route>
+                    </Routes>
+
+                    <ToTheTopButton />
+                </main>
+
+                <Footer />
+            </>
+        </AuthProvider>
     );
 }
 
