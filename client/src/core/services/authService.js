812cada4bc7ad33a678308c8d50af2e37cd57dde
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-28 21:05:01 +0200

    Add auth service

---
+++ client/src/core/services/authService.js
@@ -0,0 +1,17 @@
+import { endpoints } from '../environments/constants';
+import * as request from '../../lib/request';
+
+export const aboutMe = async () => {
+    return await request.get(endpoints.me);
+};
+
+export const login = async (email, password) => {
+    const result = await request.post(endpoints.login, { email, password });
+
+    return result;
+};
+
+export const register = async (values) =>
+    request.post(endpoints.register, values);
+
+export const logout = async () => request.get(endpoints.logout);
