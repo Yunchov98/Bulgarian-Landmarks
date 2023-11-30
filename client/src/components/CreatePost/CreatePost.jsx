6e814b9f62499209e179baf7a817837026176472
Georgi Yunchov <georgiiunchov@gmail.com>
2023-11-30 22:15:46 +0200

    Add create post component

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -1,83 +1,214 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { DevTool } from '@hookform/devtools';
+import { useFormik } from 'formik';
 
+import { CreateFormKeys, PATH } from '../../core/environments/constants';
 import { toBase64 } from '../../utils/base64';
-import * as postService from '../../services/postService';
-
+import { createValidation } from './createValidation';
+import * as postService from '../../core/services/postService';
 import styles from './CreatePost.module.css';
-import dateConverter from '../../utils/dateConverter';
+import AuthContext from '../../contexts/authContext';
+// import dateConverter from '../../utils/dateConverter';
+
+const initialValues = {
+    [CreateFormKeys.LandmarkTitle]: '',
+    [CreateFormKeys.Location]: '',
+    [CreateFormKeys.PostDescription]: '',
+    [CreateFormKeys.LandmarkDescription]: '',
+    [CreateFormKeys.PostImage]: '',
+};
 
 export default function CreatePost() {
     const navigate = useNavigate();
+    const { logoutHandler } = useContext(AuthContext);
 
-    const form = useForm({ mode: 'onBlur' });
-    const { register, control, handleSubmit, formState } = form;
-    const { errors } = formState;
+    const {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+        handleBlur,
+        isSubmitting,
+        setFieldValue,
+        touched,
+        resetForm,
+    } = useFormik({
+        initialValues,
+        onSubmit,
+        validationSchema: createValidation,
+    });
 
-    const onSubmit = async (data) => {
-        data.createdAt = dateConverter();
-
+    async function onSubmit(values) {
         try {
-            data.file = await toBase64(data.file[0]);
-            await postService.createPost(data);
+            values.postImage = await toBase64(values.postImage[0]);
 
-            navigate('/catalog');
+            await postService.createPost(values);
+
+            navigate(PATH.home);
         } catch (error) {
-            console.log(error);
+            if (error.code === 401) {
+                resetForm();
+
+                logoutHandler();
+
+                navigate(PATH.login);
+            }
         }
-    };
+    }
 
     return (
         <section className={styles['create-post']}>
             <div className={styles['content-wrapper']}>
                 <h2>Create Post</h2>
-                <form
-                    className={styles['create-form']}
-                    onSubmit={handleSubmit(onSubmit)}
-                >
+                <form onSubmit={handleSubmit} className={styles['create-form']}>
                     <div className={styles['form-item']}>
-                        <textarea
-                            {...register('description', {
-                                required: 'Description is required',
-                            })}
-                            name="description"
-                            id="description"
-                            cols="50"
-                            rows="6"
-                            placeholder="Write some description here..."
-                        ></textarea>
-                        <p className={styles['error']}>
-                            {errors.description?.message}
-                        </p>
+                        {errors[CreateFormKeys.LandmarkTitle] &&
+                            touched[CreateFormKeys.LandmarkTitle] && (
+                                <p className={styles['error']}>
+                                    {errors[CreateFormKeys.LandmarkTitle]}
+                                </p>
+                            )}
+
+                        <label htmlFor={CreateFormKeys.LandmarkTitle}></label>
+                        <input
+                            className={
+                                errors[CreateFormKeys.LandmarkTitle] &&
+                                touched[CreateFormKeys.LandmarkTitle]
+                                    ? styles['landmark-title-error']
+                                    : styles['landmark-title']
+                            }
+                            type="text"
+                            name={CreateFormKeys.LandmarkTitle}
+                            id={CreateFormKeys.LandmarkTitle}
+                            placeholder="Landmark title"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values[CreateFormKeys.LandmarkTitle]}
+                        />
                     </div>
                     <div className={styles['form-item']}>
-                        <div className={styles['file-wrapper']}>
+                        {errors[CreateFormKeys.Location] &&
+                            touched[CreateFormKeys.Location] && (
+                                <p className={styles['error']}>
+                                    {errors[CreateFormKeys.Location]}
+                                </p>
+                            )}
+                        <label htmlFor={CreateFormKeys.Location}></label>
+                        <input
+                            className={
+                                errors[CreateFormKeys.Location] &&
+                                touched[CreateFormKeys.Location]
+                                    ? styles['landmark-title-error']
+                                    : styles['landmark-title']
+                            }
+                            type="text"
+                            name={CreateFormKeys.Location}
+                            id={CreateFormKeys.Location}
+                            placeholder="Location"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values[CreateFormKeys.Location]}
+                        />
+                    </div>
+                    <div className={styles['form-item']}>
+                        {errors[CreateFormKeys.PostDescription] &&
+                            touched[CreateFormKeys.PostDescription] && (
+                                <p className={styles['error']}>
+                                    {errors[CreateFormKeys.PostDescription]}
+                                </p>
+                            )}
+                        <label htmlFor={CreateFormKeys.PostDescription}></label>
+                        <textarea
+                            className={
+                                errors[CreateFormKeys.PostDescription] &&
+                                touched[CreateFormKeys.PostDescription]
+                                    ? styles['description-error']
+                                    : styles['description']
+                            }
+                            name={CreateFormKeys.PostDescription}
+                            id={CreateFormKeys.PostDescription}
+                            cols="50"
+                            rows="6"
+                            placeholder="Post descrition... Write something about your experience there."
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values[CreateFormKeys.PostDescription]}
+                        ></textarea>
+                    </div>
+                    <div className={styles['form-item']}>
+                        {errors[CreateFormKeys.LandmarkDescription] &&
+                            touched[CreateFormKeys.LandmarkDescription] && (
+                                <p className={styles['error']}>
+                                    {errors[CreateFormKeys.LandmarkDescription]}
+                                </p>
+                            )}
+                        <label
+                            htmlFor={CreateFormKeys.LandmarkDescription}
+                        ></label>
+                        <textarea
+                            className={
+                                errors[CreateFormKeys.LandmarkDescription] &&
+                                touched[CreateFormKeys.LandmarkDescription]
+                                    ? styles['description-error']
+                                    : styles['description']
+                            }
+                            name={CreateFormKeys.LandmarkDescription}
+                            id={CreateFormKeys.LandmarkDescription}
+                            cols="50"
+                            rows="6"
+                            placeholder="Landmark description... Write something about the landmark."
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values[CreateFormKeys.LandmarkDescription]}
+                        ></textarea>
+                    </div>
+                    <div className={styles['form-item']}>
+                        {errors[CreateFormKeys.PostImage] &&
+                            touched[CreateFormKeys.PostImage] && (
+                                <p className={styles['error']}>
+                                    {errors[CreateFormKeys.PostImage]}
+                                </p>
+                            )}
+                        <div
+                            className={
+                                errors[CreateFormKeys.PostImage] &&
+                                touched[CreateFormKeys.PostImage]
+                                    ? styles['file-wrapper-error']
+                                    : styles['file-wrapper']
+                            }
+                        >
                             <span className={styles['drop-title']}>
                                 Drop files here
                             </span>
                             <i className="fa-solid fa-down-long"></i>
+                            <label htmlFor={CreateFormKeys.PostImage}></label>
                             <input
                                 className={styles['file-input']}
-                                {...register('file', {
-                                    required: 'Image is required',
-                                    minLength: 1,
-                                })}
                                 type="file"
-                                name="file"
-                                id="file"
+                                name={CreateFormKeys.PostImage}
+                                id={CreateFormKeys.PostImage}
+                                onChange={(e) =>
+                                    setFieldValue(
+                                        CreateFormKeys.PostImage,
+                                        e.target.files
+                                    )
+                                }
                             />
                         </div>
-                        <p className={styles['error']}>
-                            {errors.file?.message}
-                        </p>
                     </div>
 
                     <div className={styles['form-item']}>
-                        <button>Create Post</button>
+                        {/* <label htmlFor="inputButton"></label> */}
+                        <input
+                            className={styles['input-button']}
+                            type="submit"
+                            disabled={isSubmitting}
+                            // name="inputButton"
+                            // id="inputButton"
+                            value="Create Post"
+                        />
                     </div>
                 </form>
-                <DevTool control={control} />
             </div>
         </section>
     );
