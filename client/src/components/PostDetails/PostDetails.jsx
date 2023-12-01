f0a48dce65751b1ad755e2523ef90dcd36f17e8d
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-01 15:02:01 +0200

    Add post details component

---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,59 @@
+import dateConverter from '../../utils/dateConverter';
+import styles from './PostDetails.module.css';
+
+export default function PostDetails({
+    owner,
+    _createdOn,
+    location,
+    landmarkTitle,
+    landmarkDescription,
+    postImage,
+    onClose,
+}) {
+    return (
+        <>
+            <div onClick={onClose} className={styles['backdrop']}></div>
+            <article className={styles['post-details']}>
+                <section className={styles['user-info']}>
+                    <div className={styles['user-info-wrapper']}>
+                        <div className={styles['img-container']}>
+                            <img
+                                className={styles['user-img']}
+                                src={
+                                    owner.avatar ||
+                                    '/images/default-profile-pic.png'
+                                }
+                                alt="User profile pic"
+                            />
+                        </div>
+                        <div className={styles['post-info']}>
+                            <p className={styles['username']}>
+                                {owner.username}
+                            </p>
+                            <p className={styles['posted-on']}>
+                                Posted on {dateConverter(_createdOn)}
+                            </p>
+                            <div className={styles['location-wrapper']}>
+                                <i className="fa-solid fa-location-dot"></i>
+                                <p className={styles['posted-on']}>
+                                    {location}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className={styles['content-description']}>
+                    <p>Landmark: {landmarkTitle}</p>
+                    <p>{landmarkDescription}</p>
+                </section>
+                <section className={styles['media']}>
+                    <img
+                        src={postImage}
+                        alt={`${owner.username}'s post image`}
+                    />
+                </section>
+            </article>
+        </>
+    );
+}
