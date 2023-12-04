25af8fa257fe3e2dd019c932e2e875e376a48f2e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-04 20:56:47 +0200

    Add comment service

---
+++ client/src/core/services/commentService.js
@@ -0,0 +1,22 @@
+import { endpoints } from '../environments/constants';
+import * as requset from '../../lib/request';
+
+export const createComment = async (postId, commentData) => {
+    const result = await requset.post(endpoints.createComment, {
+        postId,
+        commentData,
+    });
+
+    return result;
+};
+
+export const getAllComments = async (postId) => {
+    const query = new URLSearchParams({
+        where: `postId="${postId}"`,
+        load: 'owner=_ownerId:users',
+    });
+
+    const result = await requset.get(`${endpoints.getAllComments}?${query}`);
+
+    return result;
+};
