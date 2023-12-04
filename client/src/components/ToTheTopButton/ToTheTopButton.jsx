c1084012b9cf58850f30c12e34d082e841d041a2
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-04 20:56:09 +0200

    Update formatting

--- client/src/components/ToTheTopButton/ToTheTopButton.jsx
+++ client/src/components/ToTheTopButton/ToTheTopButton.jsx
@@ -1,11 +1,13 @@
 import styles from './ToTheTopButton.module.css';
-
 import toTheTop from '../../utils/toTheTop';
 
-export default function () {
+export default function ToTheTopButton() {
     return (
         <div className={styles['to-the-top']}>
-            <i onClick={() => toTheTop('smooth')} className="fa-solid fa-circle-chevron-up"></i>
+            <i
+                onClick={() => toTheTop('smooth')}
+                className="fa-solid fa-circle-chevron-up"
+            ></i>
         </div>
     );
 }
