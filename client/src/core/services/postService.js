fc5947666eef5925797ef4bfbcb0af6147b15e1e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-07 16:38:28 +0200

    Add requests for likes

--- client/src/core/services/postService.js
+++ client/src/core/services/postService.js
@@ -32,3 +32,41 @@ export const editPost = async (postId, postData) => {
 export const deletePost = async (postId) => {
     return await request.remove(endpoints.deletePost(postId));
 };
+
+export const addLike = async (postId) => {
+    const result = await request.post(endpoints.likePost, { postId });
+
+    return result;
+};
+
+export const getAllLikes = async (postId) => {
+    const query = new URLSearchParams({
+        where: `postId="${postId}"`,
+        load: 'owner=_ownerId:users',
+    });
+
+    const result = await request.get(`${endpoints.likePost}?${query}`);
+
+    return result;
+};
+
+export const canLike = async (postId, userId) => {
+    return request.get(endpoints.canLike(postId, userId));
+};
+
+export const getCurrentLike = async (postId, ownerId) => {
+    const query = new URLSearchParams({
+        where: `postId="${postId}"`,
+        load: `owner=${ownerId}`,
+    });
+
+    const result = await request.get(
+        `${endpoints.getCurrentPostLikes(postId)}?${query}`
+    );
+
+    return result;
+};
+
+export const removeLike = async (likeId) => {
+    return await request.remove(endpoints.removeLike(likeId));
+};
