3335dbf77c311b6dd80da8a3184ff268e3190203
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-08 18:32:49 +0200

    Add user service

---
+++ client/src/core/services/userService.js
@@ -0,0 +1,26 @@
+import { endpoints } from '../environments/constants';
+import * as request from '../../lib/request';
+
+export const getUserPosts = async (userId) => {
+    const query = new URLSearchParams({
+        where: `_ownerId="${userId}"`,
+        load: 'owner=_ownerId:users',
+    });
+
+    const result = await request.get(
+        `${endpoints.getUserPosts(userId)}?sortBy=_createdOn%20desc&${query}`
+    );
+
+    return result;
+};
+
+export const getUserInfo = async (userId) => {
+    const query = new URLSearchParams({
+        where: `_ownerId="${userId}"`,
+    });
+    const result = await request.get(
+        `${endpoints.getCurrentUserInfo(userId)}?${query}`
+    );
+
+    return result;
+};
