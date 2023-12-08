519536ca83958e1572b86195a9705c4ebf9fa0b2
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-09 00:51:27 +0200

    Add loading spinner

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 import styles from './Catalog.module.css';
 import CatalogItem from './CatalogItem/CatalogItem';
 import * as postService from '../../core/services/postService';
+import Spinner from '../Spinner/Spinner';
 
 export default function Catalog() {
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         postService
@@ -15,17 +17,19 @@ export default function Catalog() {
             })
             .catch((err) => {
                 console.log(err);
-            });
+            })
+            .finally(() => setIsLoading(false));
     }, []);
 
     return (
         <section className={styles['catalog-section']}>
             <h2 className={styles['catalog-heading']}>Landmarks</h2>
             <div className={styles['content-wrapper']}>
+                {isLoading && <Spinner />}
                 {posts.map((post) => (
                     <CatalogItem key={post._id} {...post} />
                 ))}
-                {posts.length === 0 && (
+                {!isLoading && posts.length === 0 && (
                     <h2 className={styles['no-posts']}>No posts yet!</h2>
                 )}
             </div>
