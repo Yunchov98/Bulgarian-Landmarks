03c818d7da57849c6409c9d284968fe800568c1e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-12-09 04:06:40 +0200

    Add search form validation

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -6,6 +6,7 @@ import { PATH } from '../../core/environments/constants';
 import { SearchKeys } from '../../core/environments/constants';
 import styles from './Search.module.css';
 import * as userService from '../../core/services/userService';
+import searchValidation from './searchValidation';
 
 import Spinner from '../Spinner/Spinner';
 
@@ -17,29 +18,30 @@ export default function Search() {
     const [searchedData, setSearchedData] = useState([]);
     const [showSearchedData, setShowSearchedData] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [users, setUsers] = useState([]);
 
     const { values, handleChange, handleSubmit } = useFormik({
         initialValues,
         onSubmit,
+        validationSchema: searchValidation,
     });
 
     useEffect(() => {
         userService
             .getAllUsers()
-            .then((users) =>
-                setSearchedData(
-                    users.filter((user) =>
-                        user.username
-                            .toLowerCase()
-                            .includes(values.search.toLowerCase())
-                    )
-                )
-            )
+            .then((users) => setUsers(users))
             .catch((err) => console.log(err))
             .finally(() => setIsLoading(false));
     }, [values.search]);
 
     function onSubmit() {
+        console.log(searchedData);
+        const result = users.filter((user) =>
+            user.username
+                .toLowerCase()
+                .includes(values[SearchKeys.Search].toLowerCase())
+        );
+        setSearchedData(result);
         setShowSearchedData(true);
     }
 
@@ -77,11 +79,16 @@ export default function Search() {
                                         <img src={user.avatar} alt="" />
                                     </Link>
                                 </div>
-                                <p>{user.username}</p>
+                                {searchedData.length > 0 && (
+                                    <p>{user.username}</p>
+                                )}
                             </article>
                         ))
                     )}
                 </section>
+            )}
+            {searchedData.length === 0 && (
+                <p className={styles['no-match']}>No match</p>
             )}
         </section>
     );
